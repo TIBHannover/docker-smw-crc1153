ab8c7e412bff5e552f055bcc46e1a68736bd49a3
alexander-gesinn <alexander@gesinn.de>
2025-08-11 15:57:18 +0200

    add missing extensions

--- context/LocalSettings.SMW1153/LocalSettings.Extensions.php
+++ context/LocalSettings.SMW1153/LocalSettings.Extensions.php
@@ -1,5 +1,39 @@
 <?php
 
+## -------- CodeEditor --------
+wfLoadExtension( 'CodeEditor' );
+## ======== CodeEditor ========
+
+## -------- ConfirmAccount --------
+wfLoadExtension( 'ConfirmAccount' );
+$wgConfirmAccountRequestFormItems["Biography"]["enabled"] = false;
+$wgWhitelistRead = array( "Special:RequestAccount" );
+$wgGroupPermissions["bureaucrat"]["confirmaccount-notify"] = true;
+## ======== ConfirmAccount ========
+
+## -------- EditAccount --------
+wfLoadExtension("EditAccount");
+$wgGroupPermissions["sysop"]["editaccount"] = true;
+$wgGroupPermissions["bureaucrat"]["editaccount"] = true;
+## ======== EditAccount ========
+
+// wfLoadExtension( 'CodeEditor' );
+// wfLoadExtension( 'CodeMirror' );
+// wfLoadExtension( 'WSSlots' );
+// wfLoadExtension( 'InteractiveSemanticGraph' );
+
 ## -------- KnowledgeGraph --------
 wfLoadExtension( 'KnowledgeGraph' );
 ## ======== KnowledgeGraph ========
+
+## -------- Nuke --------
+wfLoadExtension( 'Nuke' );
+## ======== Nuke ========
+
+## -------- Scribunto --------
+wfLoadExtension( 'Scribunto' );
+## ======== Scribunto ========
+
+## -------- WikiEditor --------
+wfLoadExtension( 'WikiEditor' );
+## ======== WikiEditor ========
